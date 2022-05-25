e05bca78dba02bfc7b36c2bff85bd271be20cd7f
lbluefishl <lbluefishl@yahoo.com>
2022-05-24 20:44:46 -0700

    typed.js final

--- scripts/main.js
+++ scripts/main.js
@@ -4,32 +4,49 @@ const container = document.querySelector('.container');
 const details1 = document.querySelector('h2.left');
 const details2 = document.querySelector('h2.right');
 
+var typed = new Typed('.auto-type', {
+    strings: ['Research-based, Experience-backed Principles.'],
+    typeSpeed: 20,
+    backSpeed: 2,
+    loop: true,
+    startDelay: 1000,
+  });
+
+var typed2 = new Typed('.auto-type2', {
+    strings: ['ACT, SAT, and AP questions from official tests, organized by topic.', 'Comprehensive explanations and progress-tracking.'],
+    typeSpeed: 20,
+    backSpeed: 2,
+    loop: true,
+    startDelay: 1000,
+  });
+
 
 
 
 left.addEventListener('mouseenter', () => {
     container.classList.add('hover-left');
         details1.style.display = 'block';
-        details1.classList.add('auto-type')
+     
 });
 
 left.addEventListener('mouseleave', () => {
     container.classList.remove('hover-left');
     details1.style.display = 'none';
-    details1.classList.remove('auto-type')
-
+    typed.reset();
+    typed2.reset();
 });
 
 right.addEventListener('mouseenter', () => {
     container.classList.add('hover-right');
     details2.style.display = 'block';
-    details2.classList.add('auto-type2')
+ 
 });
 
 right.addEventListener('mouseleave', () => {
     container.classList.remove('hover-right');
     details2.style.display = 'none';
-    details2.classList.remove('auto-type2')
+    typed.reset();
+    typed2.reset();
 });
 
 
